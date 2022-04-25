aa63d4a18c0b23e39c63e32587a5440f292b0527
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-04-25 20:54:32 -0300

    Primer commit.

---
+++ TP4_Grupo10/src/main/java/ar/edu/unju/fi/tp4/Tp4Grupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo10Application.class, args);
+	}
+
+}
